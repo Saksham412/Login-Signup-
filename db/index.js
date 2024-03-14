d92e359df591cf292fae8c42ff5638daf042b40f
Saksham <sakshamshinde412@gmail.com>
2024-03-14 19:58:06 +0530

    initial commmit

---
+++ db/index.js
@@ -0,0 +1,35 @@
+const mongoose = require("require")
+
+mongoose.connect("mongodb+srv://admin:2mtgUWBXp7yVvT5J@cluster0.ohjymd8.mongodb.net/")
+
+const AdminSchema = new mongoose.Schema({
+    username:String,
+    password:String
+})
+
+const UserSchema = new mongoose.Schema({
+    username:String,
+    password:String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course"
+    }]
+})
+
+const CourseSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    imageLink: String,
+    price: Number
+})
+
+const Admin = mongoose.model('Admin', AdminSchema)
+const User = mongoose.model('User', UserSchema)
+const Course = mongoose.model('Course', CourseSchema)
+
+module.exports = {
+    Admin,
+    User,
+    Course
+}
+
